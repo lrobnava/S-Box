e70a8ac492dde1c9d18c6397de96f12d375d0ffe
luisrobertonavalara <luisrobertonavalara@gmail.com>
2020-07-20 20:32:52 -0500

    Se completo la función de autocorrelación.

--- Software/Java/Project_Java_SBox/src/Model/Representation.java
+++ Software/Java/Project_Java_SBox/src/Model/Representation.java
@@ -7,8 +7,8 @@ package Model;
 
 public class Representation {
     
-    private int m;
-    private int n;
+    private final int m;
+    private final int n;
     private int mshift;
     private int nshift;
     private int truthtable [][];
@@ -46,34 +46,31 @@ public class Representation {
     
     public void linear_combinations ()
     {
-        int m_shift = 1 << this.m;
-        int n_shift = 1 << this.n;
-	int i, j, shift;
-        
+	int i, j, shift;      
         HelperFunctions hf = new HelperFunctions();
         
 	if (n == 1) //for Boolean function there is no linear combinations
-            for (i = 0; i < m_shift; i++)
+            for (i = 0; i < this.mshift; i++)
                 this.linearcombinations[i][0] = this.truthtable[i][0]; 
 	else
 	{
-            for (i = 1; i < n_shift; i++){ // start from 1, 0 does not have any interesting case, all linear combinations
+            for (i = 1; i < this.nshift; i++){ // start from 1, 0 does not have any interesting case, all linear combinations
                 for (j = 0; j < n; j++){
                     shift = i >> j & 0x01;
                     if (1==shift) {
-                        hf.xor_elements(this.linearcombinations, 
-                                this.truthtable, 
-                                this.linearcombinations, 
-                                m, 
-                                n_shift, 
-                                m_shift);
+                        hf.xor_elements( this.linearcombinations, 
+                        this.truthtable, 
+                        this.linearcombinations, 
+                        this.m, 
+                        this.nshift, 
+                        this.mshift );
                     }
                 }
             }
 	}
     }
     
-    public int walsh_transform()
+    public int calculateWalshTransform()
     {
         int m, halfm, t1, t2, a, b, r, j;
 	int i, z, rows, columns;
@@ -106,4 +103,73 @@ public class Representation {
 	return 1;
     }
     
+    public int calculateAutocorrelationFast ()
+    {
+        int m, halfm, t1, t2, r, a, b, j;
+        int i, z, rows, columns;
+        rows = this.mshift;
+        columns = this.nshift - 1;
+
+        for (z = 0; z < columns; z++)
+        {
+            for (i = 0; i < rows; i++)
+                this.autocorrelationfast[i][z] = -1* this.walshtransform[i][z]*this.walshtransform[i][z];
+
+            for (i = 1; i <= this.m; ++i) 
+            {
+                m  = (1 << i);
+                halfm = m/2;
+                for (r = 0; r < (int) rows; r += m) 
+                {
+                    t1 = r;
+                    t2 = r + halfm;
+                    for (j = 0; j < halfm; ++j, ++t1, ++t2) 
+                    {
+                        a = this.autocorrelationfast[t1][z];
+                        b = this.autocorrelationfast[t2][z];
+                        this.autocorrelationfast[t1][z] = a + b;
+                        this.autocorrelationfast[t2][z] = a - b;
+                    }
+                }
+            }
+            for (i = 0; i < rows; i++)
+                this.autocorrelationfast[i][z] /= (1 << this.m)*(-1);
+        }
+        return 1;
+    }    
+ 
+    public int algebraic_normal_form ()
+    {
+        int tmp = 0, res = 0;
+        int i, j, k, z, rows, columns;
+
+        rows = 1 << (this.m - 1);
+        columns = this.nshift -1;
+
+        int t[] = new int[rows]; 
+        int u[] = new int[rows];
+
+        for (z = 0; z < columns; z++)
+        {
+            for (i = 0; i < (rows << 1); ++i)
+                this.algebraicnormalform[i][z] = (int) this.linearcombinations[i][z];
+            for (i = 0; i < rows; ++i)
+                u[i] = t[i] = 0;
+
+            for (i = 0; i < this.m; ++i)
+            {
+                for (j = 0; j < rows; ++j)
+                {
+                    t[j] = this.algebraicnormalform[2*j][z];
+                    u[j] = (this.algebraicnormalform[2*j][z] == this.algebraicnormalform[2*j+1][z]) ? 0 : 1;
+                }
+                for (k = 0; k < rows; ++k)
+                {
+                    this.algebraicnormalform[k][z] = t[k];
+                    this.algebraicnormalform[rows + k][z] = u[k];
+                }
+            }
+        }
+        return 1;
+    }    
 }
