d821613e66739f2795be445bb36ffd7afb626e5e
luisrobertonavalara <luisrobertonavalara@gmail.com>
2020-07-23 19:54:02 -0500

    Se realizo la función de no linealidad, peso hammming, indicador absoluto, inmunidad correlacion, suma de indicador cuadrado. Falta corregir el grado algebraico.

--- Software/Java/Project_Java_SBox/src/Model/Calculator.java
+++ Software/Java/Project_Java_SBox/src/Model/Calculator.java
@@ -11,4 +11,205 @@ package Model;
  */
 public class Calculator {
     
+    public void calculateRepresentationSBox(SBox sbox){
+        
+        int n = sbox.getN();
+        int m = sbox.getM();
+        int input[] = sbox.getSbox();
+
+        Representation representation = new Representation(n, m);
+        representation.setInput(input);
+        representation.calculateTruthTable();
+        representation.calculateLinearCombinations();
+        representation.calculateWalshTransform();
+        representation.calculateAutocorrelationFast();
+        sbox.setRepresentation(representation);
+        calculateHammingWeights(sbox);
+    }
+    
+    public int calculateNonLinearity (SBox sbox){
+        
+        int  i, j, rows, columns, value = 0, temp = 0, min = Integer.MAX_VALUE;
+        
+        rows = 1 << sbox.getM();
+        columns = 1 << sbox.getN() - 1;
+        
+        Representation represention = sbox.getRepresentation();
+        
+        int mshift = rows;
+        int walshtransformation[][] = represention.getWalshtransform();
+        
+        int components[] = new int[columns];
+        
+        for (int k = 0; k < columns; k++) {
+            components[k] = 0;
+        }
+        
+        for (j = 0; j < columns; j++){
+                if (walshtransformation[0][j] < 0)
+                    temp = walshtransformation[0][j] * (-1);
+                
+                for (i = 1; i < rows; i++){
+                    if (Math.abs(walshtransformation[i][j]) > temp)
+                            temp = Math.abs(walshtransformation[i][j]);
+                }
+                
+                components[j] = (mshift - temp) >> 1;
+                
+                if (components[j] < min){
+                    min = components[j];
+                }
+        }
+        sbox.setNonlinearity(min);
+        return min;
+    }  
+    
+    public void calculateHammingWeights(SBox sbox){
+        int m = sbox.getM();
+        int mshift = 1 << m;
+        int hamming[] = new int[mshift];
+        for(int i = 0; i < mshift; i++){
+		hamming[i] = calculateHammingWeight(i);            
+        }
+        sbox.setHammingweight(hamming);
+    }
+    
+    public int calculateHammingWeight(int x){
+        int res;
+        for (res = 0; x > 0; x = x >> 1)
+            res = res + (x & 0x01);
+        return res;
+    }
+    
+    public int calculateCorrelationImmunity(SBox sbox){
+        int  i, j, columns, order, rows, min = Integer.MAX_VALUE;
+        int m =  sbox.getM();
+        int n = sbox.getN();
+        columns = 1 << n - 1;
+        rows = 1 << m;
+        int components[] = new int[columns];
+        
+        Representation representation = sbox.getRepresentation();
+        int walshtransform[][];
+        walshtransform = representation.getWalshtransform();
+        
+        int hamming[] = sbox.getHammingweight();
+        
+        for (j = 0; j < columns; j++){
+            order = 1;
+            for (i = 1; i < rows; i++){
+                if (order == hamming[i] && walshtransform[i][j] != 0){
+                        components[j] = order - 1;
+                        break;
+                }
+                if (i == (rows - 1) && order <= m){
+                        i = 1;
+                        order++;
+                }
+                components[j] = order - 2;
+            }
+            if (components[j] < min)
+                min = components[j];
+        }
+        return min;
+    }    
+    
+    public int calculateAbsoluteIndicator(SBox sbox) {
+        int i, j, rows, columns;
+        int max = 0, temp = 0, temp2 = 0;
+
+        int m = sbox.getM();
+        int n = sbox.getN();
+        rows = 1 << m;
+        columns = 1 << n - 1;
+        
+        Representation representation = sbox.getRepresentation();
+        int autocorrelationfast[][] = representation.getAutocorrelationfast();
+        
+        int components[] = new int[columns];
+
+        for (j = 0; j < columns; j++)
+        {
+                temp = Math.abs(autocorrelationfast[1][j]); //disregard first value since it is 2^n
+                for (i = 2; i < rows; i++)	
+                {
+                        temp2 = Math.abs(autocorrelationfast[i][j]);
+                        if (temp2 > temp)
+                                temp = temp2;
+                }
+                components[j] = temp;
+                if (temp > max)
+                        max = temp;
+        }
+        return max;
+    }
+    
+    public int calculateSumOfSquareIndicator (SBox sbox) //the smaller, the bettter
+    {
+        int i, j, rows, columns;
+        int max = 0, sum = 0;
+
+        int m = sbox.getM();
+        int n = sbox.getN();
+        
+        rows = 1 << m;
+        columns = 1 << n - 1;
+        
+        Representation representation = sbox.getRepresentation();
+        int autocorrelationfast[][] = representation.getAutocorrelationfast();
+        
+        int components[] = new int[columns];
+        
+        for (j = 0; j < columns; j++)
+        {
+            for (i = 0; i < rows; i++)	
+            {
+                sum += autocorrelationfast[i][j] * autocorrelationfast[i][j];
+            }
+            components[j] = sum;
+            if (sum > max)
+                max = sum;
+            sum = 0;
+        }
+        return max;
+    }
+
+    public int calculateAlgebraicDegree (SBox sbox)
+    {
+        int  tmp, weight, deg;
+        int i, j, rows, columns, max = 0;
+
+        Representation representation = sbox.getRepresentation();
+        int algebraicnormalform[][] = representation.getAlgebraicnormalform();
+        
+        int m = sbox.getM();
+        int n = sbox.getN();
+        
+        rows = 1 << m;
+        columns = 1 << n - 1;
+        
+        int components[] = new int[columns];
+
+        for (j = 0; j < columns; j++)
+        {
+            if (algebraicnormalform[rows - 1][j] != 0)
+                deg = m;
+            else
+            {
+                for (deg = 0, i = 1; i < (rows - 1); ++i)
+                    if (algebraicnormalform[i][j] != 0) 
+                    {
+                        for (weight = 0, tmp = i; tmp > 0; tmp >>= 1)
+                            weight = weight + tmp%2;
+                        if (weight > deg)
+                            deg = weight;
+                    }
+            }
+            components[j] = deg;
+            if (components[j] > max)
+                max = components[j];
+        }
+        return max;
+    }
 }
+
--- Software/Java/Project_Java_SBox/src/Model/Representation.java
+++ Software/Java/Project_Java_SBox/src/Model/Representation.java
@@ -7,62 +7,56 @@ package Model;
 
 public class Representation {
     
-    private final int m;
-    private final int n;
+    private int m;
+    private int n;
     private int mshift;
     private int nshift;
     private int truthtable [][];
-    private final int input[];
-    private final int linearcombinations[][];
-    private final int walshtransform[][];
-    private final int autocorrelationfast[][];
-    private final int algebraicnormalform[][];
+    private int input[];
+    private int linearcombinations[][];
+    private int walshtransform[][];
+    private int autocorrelationfast[][];
+    private int algebraicnormalform[][];
     
     public Representation(int n, int m){
         this.n = n;
         this.m = m;
-        this.truthtable = new int[1 << m][1 << n];
-        this.input = new int[1 << m];
-        this.linearcombinations = new int[1 << m][1 << n - 1];
-        this.walshtransform = new int[1 << m][1 << n - 1];
-        this.autocorrelationfast = new int[1 << m][1 << n - 1];
-        this.algebraicnormalform = new int[1 << m][1 << n - 1];
+        this.nshift = 1 << n;
+        this.mshift = 1 << m;
+        this.truthtable = new int[this.mshift][this.n];
+        this.input = new int[this.mshift];
+        this.linearcombinations = new int[this.mshift][this.nshift - 1];
+        this.walshtransform = new int[this.mshift][this.nshift - 1];
+        this.autocorrelationfast = new int[this.mshift][this.nshift - 1];
+        this.algebraicnormalform = new int[this.mshift][this.nshift - 1];
     }
     
-    public int[][] getTruthTable(){
-        return this.truthtable;
-    }
-    
-    public void setTruthTable(int truthtable[][]){
-        this.truthtable = truthtable;
-    }
-    
-    public void calculateTruthTable(int m_shift, int n){
+    public void calculateTruthTable(){
     int i, j;
-    for (i = 0; i < m_shift; i++)
-        for (j = 0; j < n; j++)
-            this.truthtable[i][j] = this.input[i] >> (n - j - 1) & 0x01;
+    for (i = 0; i < this.mshift; i++)
+        for (j = 0; j < this.n; j++)
+            this.truthtable[i][j] = this.input[i] >> (this.n - j - 1) & 0x01;
     }
     
-    public void linear_combinations ()
+    public void calculateLinearCombinations ()
     {
 	int i, j, shift;      
         HelperFunctions hf = new HelperFunctions();
         
-	if (n == 1) //for Boolean function there is no linear combinations
+	if (this.n == 1) //for Boolean function there is no linear combinations
             for (i = 0; i < this.mshift; i++)
                 this.linearcombinations[i][0] = this.truthtable[i][0]; 
 	else
 	{
             for (i = 1; i < this.nshift; i++){ // start from 1, 0 does not have any interesting case, all linear combinations
-                for (j = 0; j < n; j++){
-                    shift = i >> j & 0x01;
-                    if (1==shift) {
+                for (j = 0; j < this.n; j++){
+                    shift = (i >> j) & 0x01;
+                    if (0 < shift) {
                         hf.xor_elements( this.linearcombinations, 
                         this.truthtable, 
                         this.linearcombinations, 
-                        this.m, 
-                        this.nshift, 
+                        i - 1, 
+                        j, 
                         this.mshift );
                     }
                 }
@@ -70,28 +64,23 @@ public class Representation {
 	}
     }
     
-    public int calculateWalshTransform()
-    {
+    public int calculateWalshTransform(){
         int m, halfm, t1, t2, a, b, r, j;
 	int i, z, rows, columns;
 	rows = this.mshift;
 	columns = this.nshift - 1;
 
-	for (z = 0; z < columns; z++)
-	{
+	for (z = 0; z < columns; z++){
             for (i = 0; i < rows; ++i)
                 this.walshtransform[i][z] = (this.linearcombinations[i][z] == 0) ? 1 : -1;
 
-            for (i = 1; i <= this.m; ++i) 
-            {
+            for (i = 1; i <= this.m; ++i) {
                 m  = (1 << i);
                 halfm = m/2;
-                for (r = 0; r < (int) rows; r += m) 
-                {
+                for (r = 0; r < (int) rows; r += m) {
                     t1 = r;
                     t2 = r + halfm;
-                    for (j = 0; j < halfm; ++j, ++t1, ++t2) 
-                    {
+                    for (j = 0; j < halfm; ++j, ++t1, ++t2) {
                         a = this.walshtransform[t1][z];
                         b = this.walshtransform[t2][z];
                         this.walshtransform[t1][z] = a + b;
@@ -103,28 +92,23 @@ public class Representation {
 	return 1;
     }
     
-    public int calculateAutocorrelationFast ()
-    {
+    public int calculateAutocorrelationFast (){
         int m, halfm, t1, t2, r, a, b, j;
         int i, z, rows, columns;
         rows = this.mshift;
         columns = this.nshift - 1;
 
-        for (z = 0; z < columns; z++)
-        {
+        for (z = 0; z < columns; z++){
             for (i = 0; i < rows; i++)
                 this.autocorrelationfast[i][z] = -1* this.walshtransform[i][z]*this.walshtransform[i][z];
 
-            for (i = 1; i <= this.m; ++i) 
-            {
+            for (i = 1; i <= this.m; ++i) {
                 m  = (1 << i);
                 halfm = m/2;
-                for (r = 0; r < (int) rows; r += m) 
-                {
+                for (r = 0; r < (int) rows; r += m) {
                     t1 = r;
                     t2 = r + halfm;
-                    for (j = 0; j < halfm; ++j, ++t1, ++t2) 
-                    {
+                    for (j = 0; j < halfm; ++j, ++t1, ++t2) {
                         a = this.autocorrelationfast[t1][z];
                         b = this.autocorrelationfast[t2][z];
                         this.autocorrelationfast[t1][z] = a + b;
@@ -138,8 +122,7 @@ public class Representation {
         return 1;
     }    
  
-    public int algebraic_normal_form ()
-    {
+    public int calculateAlgebraicNormalForm (){
         int tmp = 0, res = 0;
         int i, j, k, z, rows, columns;
 
@@ -149,27 +132,112 @@ public class Representation {
         int t[] = new int[rows]; 
         int u[] = new int[rows];
 
-        for (z = 0; z < columns; z++)
-        {
+        for (z = 0; z < columns; z++){
             for (i = 0; i < (rows << 1); ++i)
                 this.algebraicnormalform[i][z] = (int) this.linearcombinations[i][z];
             for (i = 0; i < rows; ++i)
                 u[i] = t[i] = 0;
 
-            for (i = 0; i < this.m; ++i)
-            {
-                for (j = 0; j < rows; ++j)
-                {
+            for (i = 0; i < this.m; ++i){
+                for (j = 0; j < rows; ++j){
                     t[j] = this.algebraicnormalform[2*j][z];
                     u[j] = (this.algebraicnormalform[2*j][z] == this.algebraicnormalform[2*j+1][z]) ? 0 : 1;
                 }
-                for (k = 0; k < rows; ++k)
-                {
+                for (k = 0; k < rows; ++k){
                     this.algebraicnormalform[k][z] = t[k];
                     this.algebraicnormalform[rows + k][z] = u[k];
                 }
             }
         }
         return 1;
+    }
+    
+    public int[][] getTruthTable(){
+        return this.truthtable;
+    }
+    
+    public void setTruthTable(int truthtable[][]){
+        this.truthtable = truthtable;
     }    
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getMshift() {
+        return mshift;
+    }
+
+    public void setMshift(int mshift) {
+        this.mshift = mshift;
+    }
+
+    public int getNshift() {
+        return nshift;
+    }
+
+    public void setNshift(int nshift) {
+        this.nshift = nshift;
+    }
+
+    public int[][] getTruthtable() {
+        return truthtable;
+    }
+
+    public void setTruthtable(int[][] truthtable) {
+        this.truthtable = truthtable;
+    }
+
+    public int[] getInput() {
+        return input;
+    }
+
+    public void setInput(int[] input) {
+        this.input = input;
+    }
+
+    public int[][] getLinearcombinations() {
+        return linearcombinations;
+    }
+
+    public void setLinearcombinations(int[][] linearcombinations) {
+        this.linearcombinations = linearcombinations;
+    }
+
+    public int[][] getWalshtransform() {
+        return walshtransform;
+    }
+
+    public void setWalshtransform(int[][] walshtransform) {
+        this.walshtransform = walshtransform;
+    }
+
+    public int[][] getAutocorrelationfast() {
+        return autocorrelationfast;
+    }
+
+    public void setAutocorrelationfast(int[][] autocorrelationfast) {
+        this.autocorrelationfast = autocorrelationfast;
+    }
+
+    public int[][] getAlgebraicnormalform() {
+        return algebraicnormalform;
+    }
+
+    public void setAlgebraicnormalform(int[][] algebraicnormalform) {
+        this.algebraicnormalform = algebraicnormalform;
+    }
+
 }
